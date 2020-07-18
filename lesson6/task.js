8a2042aea62a61e4623bcd64a86a32277ac4c38c
manemin <manemin80@gmail.com>
2020-07-18 22:41:43 +0300

    1

--- lesson6/task.js
+++ lesson6/task.js
@@ -1,3 +1,7 @@
+/* eslint-disable no-multiple-empty-lines */
+/* eslint-disable no-return-assign */
+/* eslint-disable no-param-reassign */
+/* eslint-disable prefer-object-spread */
 /* eslint-disable arrow-parens */
 /* eslint-disable max-len */
 /* eslint-disable no-plusplus */
@@ -43,7 +47,6 @@ console.log(mappedArr);
 console.log('--------------');
 // =======================================
 
-
 // ==================2=====================
 const up = (a, b) => a.toLowerCase().charCodeAt(0) - b.toLowerCase().charCodeAt(0);
 const down = (a, b) => b.toLowerCase().charCodeAt(0) - a.toLowerCase().charCodeAt(0);
@@ -75,11 +78,10 @@ console.log('--------------');
 
 // =======================================
 
-
 // =================3=====================
 // Все робити через функції масивів (foreach, map ...тд)
 // Дан масив :
-let users = [
+const users = [
     { name: 'vasya', age: 31, status: false },
     { name: 'petya', age: 30, status: true },
     { name: 'kolya', age: 29, status: true },
@@ -109,21 +111,24 @@ users.sort((a, b) => b.name.length - a.name.length);
 console.log(users);
 console.log('--------------');
 
-// - пройтись по ньому та додати кожному юзеру поле id - яке характеризує унікальний 
+// - пройтись по ньому та додати кожному юзеру поле id - яке характеризує унікальний
 // - індентифікатор (По якому принципу його створювати - ваше рішення), та зберегти
 // - це в новий масив (первинний масив залишиться без змін)
 
 const addedId = users.map(key => Object.assign({}, key));
 addedId.forEach((key, index) => key.id = ++index);
-
-console.log('old arr:', users);
 console.log('new arr:', addedId);
 console.log('--------------');
 
 // - відсортувати його за індентифікатором
-
+addedId.sort((a, b) => b.id - a.id);
+console.log('sorted:', addedId);
+console.log('--------------');
 
 // -- наисать функцию калькулятора с 2мя числами и колбеком
+function calculator() {
+}
+
 // -- наисать функцию калькулятора с 3мя числами и колбеком
 
 // =============================================
@@ -131,8 +136,6 @@ console.log('--------------');
 // =============КЛАССНАЯ РАБОТА=================
 // =============================================
 // =============================================
-
-
 
 
 // let cars = [ {producer:"subaru",model: "wrx",year: 2010, color:"blue",type: "sedan",engine: "ej204x",volume: 2,power: 400}, {producer:"subaru",model: "legacy",year: 2007, color:"silver",type: "sedan",engine: "ez30",volume: 3,power: 250}, {producer:"subaru",model: "tribeca",year: 2011, color:"white",type: "jeep",engine: "ej20",volume: 2,power: 300}, {producer:"subaru",model: "leone",year: 1998, color:"yellow",type: "sedan",engine: "ez20x",volume: 2,power: 140}, {producer:"subaru",model: "impreza",year: 2014, color:"red",type: "sedan",engine: "ej204x",volume: 2,power: 200}, {producer:"subaru",model: "outback",year: 2014, color:"red",type: "hachback",engine: "ej204",volume: 2,power: 165}, {producer:"bmw",model: "115",year: 2013, color:"red",type: "hachback",engine: "f15",volume: 1.5,power: 120}, {producer:"bmw",model: "315",year: 2010, color:"white",type: "sedan",engine: "f15",volume: 1.5, power: 120}, {producer:"bmw",model: "650",year: 2009, color:"black",type: "coupe",engine: "f60",volume: 6,power: 350}, {producer:"bmw",model: "320",year: 2012, color:"red",type: "sedan",engine: "f20",volume: 2,power: 180}, {producer:"mercedes",model: "e200",year: 1990, color:"silver",type: "sedan",engine: "eng200",volume: 2,power: 180}, {producer:"mercedes",model: "e63",year 2017, color:"yellow",type: "sedan",engine: "amg63",volume:3,power: 400}, {producer:"mercedes",model: "c250",year: 2017, color:"red",type: "sedan",engine: "eng25",volume: 2.5,power: 230} ];
