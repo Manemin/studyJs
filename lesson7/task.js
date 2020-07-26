86ae258c23cbd0b9cf126a6e107f6962b22f6d05
manemin <manemin80@gmail.com>
2020-07-26 20:37:29 +0300

    add 12

--- lesson7/task.js
+++ lesson7/task.js
@@ -1,3 +1,4 @@
+/* eslint-disable object-curly-newline */
 /* eslint-disable prefer-destructuring */
 /* eslint-disable no-param-reassign */
 /* eslint-disable no-plusplus */
@@ -243,22 +244,22 @@ btnStr.onclick = () => {
 
 // =========================================
 
-
+// =================12======================
 
 // -- взять массив пользователей
-// let usersWithAddress = [
-//                 {id:1,name: 'vasya', age: 31, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 16}},
-//                 {id:2,name: 'petya', age: 30, status: true, address: {city: 'Kyiv', street: 'Shevchenko', number: 1}},
-//                 {id:3,name: 'kolya', age: 29, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 121}},
-//                 {id:4,name: 'olya', age: 28, status: false, address: {city: 'Ternopil', street: 'Shevchenko', number: 90}},
-//                 {id:5,name: 'max', age: 30, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 115}},
-//                 {id:6,name: 'anya', age: 31, status: false, address: {city: 'Kyiv', street: 'Shevchenko', number: 2}},
-//                 {id:7,name: 'oleg', age: 28, status: false, address: {city: 'Ternopil', street: 'Shevchenko', number: 22}},
-//                 {id:8,name: 'andrey', age: 29, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 43}},
-//                 {id:9,name: 'masha', age: 30, status: true, address: {city: 'Kyiv', street: 'Shevchenko', number: 12}},
-//                 {id:10,name: 'olya', age: 31, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 16}},
-//                 {id:11,name: 'max', age: 31, status: true, address: {city: 'Ternopil', street: 'Shevchenko', number: 121}}
-//             ];
+const usersWithAddress = [
+    { id: 1, name: 'vasya', age: 31, status: false, address: { city: 'Lviv', street: 'Shevchenko', number: 16 } },
+    { id: 2, name: 'petya', age: 30, status: true, address: { city: 'Kyiv', street: 'Shevchenko', number: 1 } },
+    { id: 3, name: 'kolya', age: 29, status: true, address: { city: 'Lviv', street: 'Shevchenko', number: 121 } },
+    { id: 4, name: 'olya', age: 28, status: false, address: { city: 'Ternopil', street: 'Shevchenko', number: 90 } },
+    { id: 5, name: 'max', age: 30, status: true, address: { city: 'Lviv', street: 'Shevchenko', number: 115 } },
+    { id: 6, name: 'anya', age: 31, status: false, address: { city: 'Kyiv', street: 'Shevchenko', number: 2 } },
+    { id: 7, name: 'oleg', age: 28, status: false, address: { city: 'Ternopil', street: 'Shevchenko', number: 22 } },
+    { id: 8, name: 'andrey', age: 29, status: true, address: { city: 'Lviv', street: 'Shevchenko', number: 43 } },
+    { id: 9, name: 'masha', age: 30, status: true, address: { city: 'Kyiv', street: 'Shevchenko', number: 12 } },
+    { id: 10, name: 'olya', age: 31, status: false, address: { city: 'Lviv', street: 'Shevchenko', number: 16 } },
+    { id: 11, name: 'max', age: 31, status: true, address: { city: 'Ternopil', street: 'Shevchenko', number: 121 } },
+];
 // Создать три чекбокса. Каждый из них активирует фильтр для вышеуказаного массива.
 // Фильтры могут работать как вместе так и по отдельности.
 // 1й - отфильтровывает пользователей со статусом false (осталяет со статусом false)
@@ -266,7 +267,20 @@ btnStr.onclick = () => {
 // 3й - оставляет тех у кого город киев
 // Данные выводить в документ
 
+const chkBoxAge = document.forms.users.age;
+const chkBoxCity = document.forms.users.city;
+const chkBoxStatus = document.forms.users.status;
+const divResult = document.getElementById('result');
 
+function filterStatus() {
+    if (chkBoxStatus.checked) {
+        usersWithAddress.filter(obj => !obj.status);
+    }
+    
+}
+
+
+// =========================================
 
 // *****(Прям овердоз с рекурсией) Создать функцию которая принимает
 //  какой-либо элемент DOM-структуры. Функция создает в боди 2 кнопки (назад/вперед)
